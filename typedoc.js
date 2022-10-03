f6d3c28737db9a1f557c54354e0f18effe6bec6c
Paul Gerarts <paulgerarts@weave.nl>
2022-10-03 14:11:46 +0200

    chore: update dependencies and create a v1 branch

--- typedoc.js
+++ typedoc.js
@@ -1,13 +1,11 @@
 module.exports = {
     readme: 'README.md',
     tsconfig: 'tsconfig.json',
-    mode: 'modules',
-    ignoreCompilerErrors: true,
     exclude: [
         '**/node_modules/**',
         '**/test/**',
         '**/*.test.*'
     ],
     out: './docs',
-    includes: 'lib/',
+    entryPoints: 'lib/ReduxAsync.ts'
 };
